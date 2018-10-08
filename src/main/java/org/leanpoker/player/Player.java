567ddc2b4cc34e9ab31d2b92d02ff946e8979f0a
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-08 14:27:53 +0200

    First round methods CD.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -23,20 +23,29 @@ public class Player {
         JsonObject game = request.getAsJsonObject();
         Player player = new Player();
         player.setGameStatus(game);
+        return player.game(game);
+    }
 
+    private int getMinmumRaise(JsonObject game) {
+        return game.get("minimum_raise").getAsInt();
+    }
 
-
-        return player.zeroRoundBehaviour(game);
+    private int game(JsonObject game){
+        if (round == 0){
+            return zeroRoundBehaviour(game);
+        }
+        return notFirstRoundBehaviour();
     }
 
     private void setGameStatus(JsonObject game) {
         round = game.get("round").getAsInt();
         currentBuyIn = game.get("current_buy_in").getAsInt();
-        minimumRaise = game.get("minimum_raise").getAsInt();
+        minimumRaise = getMinmumRaise(game);
         currentBet = getCurrentBet(game);
         stack = getStack(game);
         holeCards = getHoleCards(game);
         communityCards = getCommunityCards(game);
+
     }
 
     private int getCurrentBet(JsonObject game) {
@@ -138,6 +147,92 @@ public class Player {
         int bet = 0;
         if (minimumRaise<stack/percent*100) bet = stack / percent * 100;
         return bet;
+    }
+
+    private int notFirstRoundBehaviour(){
+        return whatBet();
+    }
+
+
+    private boolean highCard(){
+        int highestCard = holeCards.get(0).getRank();
+        if (highestCard < holeCards.get(1).getRank()) highestCard = holeCards.get(1).getRank();
+        for (Card card : communityCards){
+            if (highestCard < card.getRank()) return false;
+        }
+        return true;
+    }
+
+    private boolean pair(){
+
+        for (Card card : holeCards){
+            for(Card tableCard : communityCards){
+                if (card.getRank() == tableCard.getRank()) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean three() {
+        int counter;
+        for (Card card : holeCards){
+            counter = 0;
+            for(Card tableCard : communityCards){
+                if (card.getRank() == tableCard.getRank()) {
+                    counter++;
+                    if(counter == 3) {
+                        return true;
+                    }
+                };
+            }
+        }
+        return false;
+    }
+
+    private boolean quads() {
+        int counter;
+        for (Card card : holeCards){
+            counter = 0;
+            for(Card tableCard : communityCards){
+                if (card.getRank() == tableCard.getRank()) {
+                    counter++;
+                    if(counter == 4) {
+                        return true;
+                    }
+                };
+            }
+        }
+        return false;
+    }
+
+    private int whatBet() {
+
+//        if(poker()) {
+//
+//            return raiseByPercent(100);
+        if (quads()) {
+
+            return raiseByPercent(100);
+//        } else if (full()) {
+//
+//            return raiseByPercent(100);
+//        } else if (colour()) {
+//
+//            return raiseByPercent(60);
+//        } else if (straight()) {
+//
+//            return raiseByPercent(60);
+        } else if (three()) {
+
+            return raiseByPercent(40);
+        } else if (pair()) {
+
+            return raiseByPercent(20);
+        } else if (highCard()) {
+
+            return raiseByPercent(10);
+        }
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
