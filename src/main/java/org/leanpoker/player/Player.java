8bd31d6f9c84246c5a9456fc4c5c93e08a6144d4
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-08 10:24:58 +0200

    BET 5

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 5;
+        return 15;
     }
 
     public static void showdown(JsonElement game) {
