19b972904b8369994bfb22f5f2d1bbd053d18cd1
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-08 11:49:56 +0200

    First round methods CD.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -31,6 +31,14 @@ public class Player {
 
 //    private int zeroRoundBehaviour(){
 //        int bet = 0;
+//        bet = zeroRoundPairBet();
+//        if (bet == 0) bet = zeroRoundSameColorBet();
+//        if (bet == 0) bet = zeroRoundHighCardBet();
+//        return bet;
+//    }
+//
+//    private int zeroRoundPairBet(){
+//        int bet = 0;
 //        if (firstCardrank.equals(secondCardrank)){
 //            if (stack < 10*minimal_raise){
 //                bet = stack;
@@ -39,10 +47,31 @@ public class Player {
 //                bet = 10 * minimal_raise;
 //            }
 //        }
+//        return bet;
+//    }
+//
+//    private int zeroRoundSameColorBet(){
+//        int bet = 0;
 //        if (firstCardSuit.equals(secondCardSuite)){
-//            if (minimal_raise<stack/10) bet = stack / 10;
-//            else bet = 0;
+//            bet = raiseByPercent(10);
 //        }
+//        return bet;
+//    }
+//
+//    private int zeroRoundHighCardBet(){
+//        int bet = 0;
+//        if(firstCardRank > 10 || secondCardRank > 10){
+//            bet = raiseByPercent(10);
+//        }
+//        if(firstCardRank > 12 || secondCardRank > 10){
+//            bet = raiseByPercent(15);
+//        }
+//        return bet;
+//    }
+//
+//    private int raiseByPercent(int percent){
+//        int bet = 0;
+//        if (minimal_raise<stack/percent) bet = stack / percent;
 //        return bet;
 //    }
 
