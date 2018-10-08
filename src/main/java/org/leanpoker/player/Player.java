b5efa0ebde9fa756f77ba47ae5f4f25e21ab159c
Bartlomiej Zadlo <bartlomiejzadlo@gmail.com>
2018-10-08 10:39:07 +0200

    BET 999

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 150;
+        return 999;
     }
 
     public static void showdown(JsonElement game) {
