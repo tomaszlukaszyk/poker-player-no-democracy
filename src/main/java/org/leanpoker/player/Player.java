9e72734d2d17a92d0b9ac04c87d6c260dba7cc40
Tomasz Łukaszyk <lukaszyk84@gmail.com>
2018-10-08 10:54:10 +0200

    First json parse

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,8 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,6 +11,21 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        JsonObject game = request.getAsJsonObject();
+        int minimumRaise = game.get("minimum_raise").getAsInt();
+        System.out.println(minimumRaise);
+
+        JsonArray players = game.getAsJsonArray("players");
+
+        players.forEach(player -> {
+            JsonObject currentPlayer = player.getAsJsonObject();
+            String playerName = currentPlayer.get("name").getAsString();
+
+            if (playerName.equals("NO democracy")) {
+                System.out.println(playerName);
+            }
+        });
+
         return 999;
     }
 
